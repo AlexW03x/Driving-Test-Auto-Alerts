19c1e6a22d2edd4ee977cad00494c95a758224ff
Alex Walker <106552874+AlexW03x@users.noreply.github.com>
2024-07-24 19:44:28 +0100

    Began expanding on the webScraper object class

--- Driving Test Alerts/main.py
+++ Driving Test Alerts/main.py
@@ -1,5 +1,10 @@
+from webScraper import *
+
 def main():
-    print("Hello World!")
+    # driving class instance called, |Test Type|Driving License ID|Date|Location|ScriptCooldown
+    drivingWebsite = webScrape("Car", "WALKE009303AP9SM", "unset", "unset", 30)
+    checkDVLA = webScrape.checkWebsiteResponse("https://driverpracticaltest.dvsa.gov.uk/application?execution=e2s1")
+    print(f"{checkDVLA}")
     
 if __name__ == "__main__":
     main()
--- Driving Test Alerts/webScraper.py
+++ Driving Test Alerts/webScraper.py
@@ -2,9 +2,63 @@ import requests
 from bs4 import BeautifulSoup
 
 class webScrape:
-    def __init__(self):
-        self.testType = ""
-        self.drivingLicenseID = ""
-        self.preferredTestDate = ""
-        self.scriptCooldown = 0
+    def __init__(self, choiceOfTest, drivingID, date, location, cooldown):
+        self.testType = choiceOfTest
+        self.drivingLicenseID = drivingID
+        self.preferredTestDate = date
+        self.postCode = location
+        self.scriptCooldown = cooldown
+        
+    def checkWebsiteResponse(websiteURL):
+        try:
+            web = requests.get(websiteURL)
+            web.raise_for_status()
+            return web.text
+        except:
+            return f"Couldn't fetch url: {websiteURL}"
+        
+    def identifyWebElement(htmlCode, element):
+        try:
+            return BeautifulSoup(htmlCode, 'html.parser').find(element).get_text(strip=True)
+        except:
+            return "Error in parsing website!"
+        
+    def identifyWebElementWithID(htmlCode, element, ID):
+        try:
+            return BeautifulSoup(htmlCode, 'html.parser').find(element, {"id": ID}).get_text(strip=True)
+        except:
+            return "Error in parsing website!"
+        
+        
+    #getters
+    def getTestType(self):
+        return self.testType
+    
+    def getDrivingLicense(self):
+        return self.drivingLicenseID
+    
+    def getTestDate(self):
+        return self.preferredTestDate
+    
+    def getTestLocation(self):
+        return self.postCode
+    
+    def getScriptCooldown(self):
+        return self.scriptCooldown
+        
+    #setters
+    def changeTest(self, choiceOfTest):
+        self.testType = choiceOfTest
+        
+    def changeID(self, drivingID):
+        self.drivingLicenseID = drivingID
+    
+    def changeDate(self, date):
+        self.preferredTestDate = date
+    
+    def changeLocation(self, location):
+        self.postCode = location
+        
+    def changeCooldown(self, cooldown):
+        self.scriptCooldown = cooldown
         
