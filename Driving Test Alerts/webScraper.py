29dc0793c7e93b2b188af53115b9d039f486ad29
Alex Walker <106552874+AlexW03x@users.noreply.github.com>
2024-07-27 19:40:21 +0100

    Cleaned up the file

--- Driving Test Alerts/webScraper.py
+++ Driving Test Alerts/webScraper.py
@@ -1,5 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -14,26 +12,6 @@ class webScrape:
         self.scriptCooldown = cooldown
         
     #website functions
-        
-    def checkWebsiteResponse(websiteURL, header):
-        try:
-            web = requests.get(websiteURL, header)
-            web.raise_for_status()
-            return web.text
-        except:
-            return f"Couldn't fetch url: {websiteURL}"
-        
-    def identifyWebElement(htmlCode, element):
-        try:
-            return BeautifulSoup(htmlCode, 'html.parser').find(element).get_text(strip=True)
-        except:
-            return "Error in parsing website!"
-        
-    def identifyWebElementWithID(htmlCode, element, ID):
-        try:
-            return BeautifulSoup(htmlCode, 'html.parser').find(element, {"id": ID}).get_text(strip=True)
-        except:
-            return "Error in parsing website!"
         
     #getters
     def getTestType(self):
