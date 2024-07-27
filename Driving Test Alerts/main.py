099326cd98207d766cbab1c6ccbfedaff20fc8ee
Alex Walker <106552874+AlexW03x@users.noreply.github.com>
2024-07-27 19:39:04 +0100

    Working with undetected seleniumbase chrome driver and began putting the script together

--- Driving Test Alerts/main.py
+++ Driving Test Alerts/main.py
@@ -1,10 +1,62 @@
+from seleniumbase import BaseCase
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
 from webScraper import *
+from time import *
+from seleniumbase import SB
+from selenium.webdriver.common.action_chains import ActionChains
 
-def main():
-    # driving class instance called, |Test Type|Driving License ID|Date|Location|ScriptCooldown
-    drivingWebsite = webScrape("Car", "WALKE009303AP9SM", "unset", "unset", 30)
-    checkDVLA = webScrape.checkWebsiteResponse("https://driverpracticaltest.dvsa.gov.uk/application?execution=e2s1")
-    print(f"{checkDVLA}")
+#this is to identify all of the components involved in BaseCase and SB
+class Scrape(BaseCase):
+    def test(self):
+        self.uc_open("")
+        
+
+
+#main script we will be running
+with SB(uc = True) as sb:
+    sb.uc_open("https://www.gov.uk/book-driving-test")
     
-if __name__ == "__main__":
-    main()
+    sb.wait_for_element_visible("body", timeout=6)
+    sb.scroll_into_view(By.LINK_TEXT, "Start now")
+    sleep(1)
+    sb.click_link("Start now", timeout=3)
+    sb.wait_for_element_visible("body", timeout=15)
+    sleep(1)
+    #check if the authentication page has popped up
+    if(sb.is_element_visible("#main-iframe") == True):
+        print("Authentication required!")
+        sb.switch_to_frame("#main-iframe")
+        sb.click('span:contains("Click to verify")')
+        
+        try: #prevent crashing errors
+            sb.wait_for_element_visible(".geetest_slider_track", timeout=10) #waits for puzzle slider to become visible
+            if(sb.is_element_visible(".geetest_slider_button") == True):
+                print("Puzzle completion required!") #debug
+                slideAction = ActionChains(sb.driver)
+                
+                for i in range(0,10): #width from 0 to 100
+                    if(sb.is_element_visible(".geetest_slider_button") == False):
+                        print("Puzzle disappeared!") #debug
+                        break
+                    else:
+                        slideAction.click_and_hold(sb.find_element(".geetest_slider_button")).move_by_offset(i * 10, 0).release().perform()
+                        sleep(1.25) #ensures that puzzle gets to confirm
+                
+                if(sb.is_element_visible(".geetest_radar_tip") == True):
+                    sb.click('span:contains("Retry")') #If puzzle fails just reclick to bypass.
+        except:
+            print("No puzzle or captcha found...")
+            pass
+            
+        sb.switch_to_default_content()
+    sleep(1)
+    sb.wait_for_element_visible("body", timeout=6)
+    try:
+        sb.find_element_by_id("test-type-car").click()
+    except:
+        pass
+    sleep(2000)
+    
+
+    
