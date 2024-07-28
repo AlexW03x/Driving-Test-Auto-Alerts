bacab5e121b9b86a3c7d7d79cbf98c15c5a594b9
Alex Walker <106552874+AlexW03x@users.noreply.github.com>
2024-07-28 21:29:08 +0100

    Changed the inputUserSettings and updated all objects to support email

--- Driving Test Alerts/main.py
+++ Driving Test Alerts/main.py
@@ -2,43 +2,56 @@ from webScraperSettings import *
 from webScraperScript import *
 
 global scraperSettings #Public class object call
-scraperSettings = webScraperSettings("car (manual and automatic)", "", "", "", "")
+scraperSettings = webScraperSettings("car (manual and automatic)", "", "", "", "", 0)
         
 def main():
     print("Welcome to Alex's Driving Test Availability Reader!")
     print("---------------------------------------------------")
     print("Please enter the details that will be prompted.")
     
-    [licenseID, date, postcode, cooldown] = inputUserSettings()
-
-    scraperSettings.changeID(licenseID)
-    scraperSettings.changeDate(date)
-    scraperSettings.changeLocation(postcode)
-    scraperSettings.changeCooldown(cooldown)
+    inputUserSettings()
+    
+    print("The script will now begin!")
+    executeScript(scraperSettings.getDrivingLicense(),
+                  scraperSettings.getTestDate(),
+                  scraperSettings.getTestLocation(),
+                  scraperSettings.getEmails(),
+                  scraperSettings.getScriptCooldown())
     
     
-    
-    
-
 def inputUserSettings():
     licenseID = ""
     date = ""
     postcode = ""
+    email = False
     cooldown = 0
-    while(licenseID == "" and date == "" and postcode == "" and cooldown == 0):
+    while(licenseID == "" and date == "" and postcode == "" and email == False and cooldown == 0):
         try:
             if(licenseID == ""):
                 licenseID = str(input("Driving License ID: "))
+                scraperSettings.changeID(licenseID)
+                
             if(date == ""):
                 date = str(input("Preferred Test Date (dd/mm/yy): "))
+                scraperSettings.changeDate(date)
+                
             if(postcode == ""):
                 postcode = str(input("Post code, Example ABC DEF: "))
+                scraperSettings.changeLocation(postcode)
+                
+            if(email == False):
+                getEmails = str(input("Recieve email alerts to your email? (Y/N): "))
+                if("y" in getEmails.lower() or "yes" in getEmails.lower()):
+                    email = True
+                    scraperSettings.changeGetEmail(email)
+                
             if(cooldown ==  0):
+                print("Terminal script cooldown per requests: 5 minutes!")
                 cooldown = 5 * 60 #5 minutes per entry requests 
+                scraperSettings.changeCooldown(cooldown)
         except:
             pass
         
-    return [licenseID, date, postcode, cooldown]
     
     
     
